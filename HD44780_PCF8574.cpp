61ae1b981dacae0fbda01ee8dcbae5544dceb042
Bruce MacKinnon <bruce@mackinnon.com>
2023-11-16 21:46:30 -0500

    Working on reading

--- HD44780_PCF8574.cpp
+++ HD44780_PCF8574.cpp
@@ -55,10 +55,27 @@ void HD44780_PCF8574::_write8(bool rsBit, uint8_t d) {
 /*
 4 bits of LCD data are returned in the LSB bits of this
 funciton's return.
+
+From data sheet [pg 7]:
+
+Input HIGH: The master needs to write 1 to the register to set 
+the port as an input mode if the device is not in the default 
+power-on condition. The master reads the register to check the 
+input status. If the external source pulls the port pin up to 
+VDD or drives logic 1, then the master will read the value of 1.
+
+Input LOW: The master needs to write 1 to the register to set 
+the port to input mode if the device is not in the default 
+power-on condition. The master reads the register to check the 
+input status. If the external source pulls the port pin down to 
+VSS or drives logic 0, which sinks the weak 100 A current source, 
+then the master will read the value of 0.
+
 */
 uint8_t HD44780_PCF8574::_read8(bool rsBit) {
-    // Turn on the read bit
-    uint8_t w0 = RWBIT;
+    // Turn on the read bit and keep the 4 data bits high to 
+    // allow reading.
+    uint8_t w0 = 0b1111 | RWBIT;
     // Turn on the backlight if requested
     if (_backLight) {
         w0 |= BLBIT;
